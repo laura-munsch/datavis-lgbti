2f5baf93da5f336c3c2fa844a7190c932a43d676
Laura Munsch <laura.munsch@etu.unistra.fr>
2020-11-22 18:38:29 +0100

    stockage de toutes les données dans un tableau

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     'use strict';
 
+    const allData = [];
+
     const srcOpenness               = './data/openness.csv';
     const srcOpenAtSchool           = './data/open-at-school.csv';
     const srcSchoolAdressingIssues  = './data/school-adressing-lgbti-issues.csv';
@@ -46,6 +48,15 @@ window.onload = function() {
             .attr('stroke', stroke)
     }
 
+    function logData(data, width, height, size) {
+        allData.push({
+            data: data,
+            width: width,
+            height: height,
+            size: size
+        });
+    }
+
     function drawData(data, width, height, size) {
         let x = width;
         let y = height;
@@ -60,123 +71,114 @@ window.onload = function() {
 
     d3.csv(srcOpenness).then((data, error) => {
         if (error) throw error;
-
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'Very open');
-        drawData(data, canvas.width / 2, 180, 150);
+        logData(data, canvas.width / 2, 180, 150);
     });
-
-    ////// SCHOOL
 
     d3.csv(srcOpenAtSchool).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'Very open');
-        drawData(data, (canvas.width / 2) - 290, 340, 100);
+        logData(data, (canvas.width / 2) - 290, 340, 100);
     });
 
     d3.csv(srcSchoolAdressingIssues).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'No');
         data.forEach(d => {
             d.percentage = 100 - d.percentage;
         });
-        drawData(data, (canvas.width / 2) - 450, 200, 75);
+        logData(data, (canvas.width / 2) - 450, 200, 75);
     });
 
     d3.csv(srcLeavingSchool).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'Yes');
-        drawData(data, (canvas.width / 2) - 490, 380, 75);
+        logData(data, (canvas.width / 2) - 490, 380, 75);
     });
 
     d3.csv(srcNegativeCommentsSchool).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'Never');
         data.forEach(d => {
             d.percentage = 100 - d.percentage;
         });
-        drawData(data, (canvas.width / 2) - 370, 530, 75);
+        logData(data, (canvas.width / 2) - 370, 530, 75);
     });
 
     d3.csv(srcOpenToSchoolmates).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'All');
-        drawData(data, (canvas.width / 2) - 180, 520, 75);
+        logData(data, (canvas.width / 2) - 180, 520, 75);
     });
 
     d3.csv(srcSupportSchool).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'Never');
         data.forEach(d => {
             d.percentage = 100 - d.percentage;
         });
-        drawData(data, (canvas.width / 2) - 420, 665, 50);
+        logData(data, (canvas.width / 2) - 420, 665, 50);
     });
-
-    ////////////// WORK
 
     d3.csv(srcOpenAtWork).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'Very open');
-        drawData(data, (canvas.width / 2) + 290, 340, 100);
+        logData(data, (canvas.width / 2) + 290, 340, 100);
     });
 
     d3.csv(srcDiscriminationWork).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'Yes');
-        drawData(data, (canvas.width / 2) + 450, 200, 75);
+        logData(data, (canvas.width / 2) + 450, 200, 75);
     });
 
     d3.csv(srcOpenToColleagues).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'All');
-        drawData(data, (canvas.width / 2) + 490, 380, 75);
+        logData(data, (canvas.width / 2) + 490, 380, 75);
     });
 
     d3.csv(srcOpenToSuperiors).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'All');
-        drawData(data, (canvas.width / 2) + 370, 530, 75);
+        logData(data, (canvas.width / 2) + 370, 530, 75);
     });
 
     d3.csv(srcOpenToClients).then((data, error) => {
         if (error) throw error;
 
-        // traitement des données...
-        console.log(data);
         data = answer(data, 'All');
-        drawData(data, (canvas.width / 2) + 180, 520, 75);
+        logData(data, (canvas.width / 2) + 180, 520, 75);
+    });
+
+    Promise.all([
+        d3.csv(srcOpenness),
+        d3.csv(srcOpenAtSchool),
+        d3.csv(srcSchoolAdressingIssues),
+        d3.csv(srcLeavingSchool),
+        d3.csv(srcNegativeCommentsSchool),
+        d3.csv(srcOpenToSchoolmates),
+        d3.csv(srcSupportSchool),
+        d3.csv(srcOpenAtWork),
+        d3.csv(srcDiscriminationWork),
+        d3.csv(srcOpenToColleagues),
+        d3.csv(srcOpenToSuperiors),
+        d3.csv(srcOpenToClients)
+    ]).then(() => {
+        console.log(allData);
+        allData.forEach(d => {
+            drawData(d.data, d.width, d.height, d.size);
+        });
     });
 
 }
